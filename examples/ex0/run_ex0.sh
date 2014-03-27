bc69b41fc31557448568afdd310944da1c73e0c1
filipe.manuel.vasconcelos@gmail.com <filipe.manuel.vasconcelos@gmail.com@e3ff7af2-d1ba-a1ce-caeb-a33717494603>
2014-03-27 14:33:43 +0000

    ex0

--- examples/ex0/run_ex0.sh
+++ examples/ex0/run_ex0.sh
@@ -5,8 +5,17 @@ echo $sep
 echo "#Example 0: Minimum settings with FCC structure"
 echo "#just look at control.F"
 echo $sep
-echo "4x4x4 FCC LJ a=6.0000000000"
+echo "FCC LJ a=1.5414"
 echo $sep
-$EXE control.F > stdout.4x4x4 
-grep Etot stdout.4x4x4 
+$EXE control.F > stdout 
+grep natm stdout
+etot=`grep Etot stdout | awk '{print $NF}'` 
+etot2=`echo ${etot} | sed -e 's/[eE]+*/\\*10\\^/'`
+natm=`grep natm stdout | awk '{print $NF}'`
+lr=`grep "long range correction :" stdout |  awk '{print $NF}'`
+etotr=`echo "scale=8;$etot2/$natm" | bc -l`
+etotlr=`echo "scale=8;($etot2+($lr))/$natm " | bc -l`
+echo "Etot                  = " $etot
+echo "Etot/atom             = " $etotr
+echo "Etot/atom + lr corre. = " $etotlr
 exit 0
