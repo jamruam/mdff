9cb2feed9926f22641806d3f6f6381f6c2304376
filipe.manuel.vasconcelos@gmail.com <filipe.manuel.vasconcelos@gmail.com@e3ff7af2-d1ba-a1ce-caeb-a33717494603>
2014-03-27 22:10:19 +0000

    ex8

---
+++ examples/ex8/run_ex8.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+EXEDLPOLY=
+EXEMDFF=mdff.x
+
+dlpoly=false
+mdff=true
+ensemble=npt
+
+if $dlpoly; then
+	echo "running DL_POLY test (ex8)"
+	cd dl_poly
+		rm STATIS OUTPUT RDFDAT REVCON  REVIVE
+		$EXEDLPOLY 
+		tail -n +4 RDFDAT > RDFDAT.$ensemble
+	cd ..
+fi
+
+if $mdff; then
+	echo "running mdff test (ex8)"
+	cd mdff
+	$EXEMDFF control_md.F > stdout_md.$ensemble
+	$EXEMDFF control_gr.F > stdout_gr.$ensemble
+	mv GRTFF GRTFF.$ensemble
+	cd ..
+fi
+
+cat > plot.gr << eof
+#!/usr/bin/gnuplot -persist
+reset
+p 'dl_poly/RDFDAT.$ensemble' w l ,'mdff/GRTFF.$ensemble' w l
+eof
+chmod u+x plot.gr
+./plot.gr
+
