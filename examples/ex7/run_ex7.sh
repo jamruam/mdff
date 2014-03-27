0cb5ca5c463160e0d4673aa2e9ec6623e091920a
filipe.manuel.vasconcelos@gmail.com <filipe.manuel.vasconcelos@gmail.com@e3ff7af2-d1ba-a1ce-caeb-a33717494603>
2014-03-27 21:20:23 +0000

    ex7

--- examples/ex7/run_ex7.sh
+++ examples/ex7/run_ex7.sh
@@ -1,58 +1,34 @@
 #!/bin/bash
 
-EXEDLPOLY=/home/filipe/recherche/dl_class_1.9/execute/DLPOLY.Y
-EXEMDFF=/home/filipe/recherche/mdff/bin/mdff.x
-EXEMOLDY=/home/filipe/recherche/moldy/moldy
+EXEDLPOLY=
+EXEMDFF=mdff.x
 
 dlpoly=false
-moldy=false
 mdff=true
+ensemble=nve
 
 if $dlpoly; then
 	echo "running DL_POLY test (ex7)"
 	cd dl_poly
-	rm STATIS OUTPUT RDFDAT REVCON  REVIVE
-	$EXEDLPOLY
-	tail -n +4 RDFDAT > tmp 
-	mv tmp RDFDAT 
-	tail -n +3 STATIS > tmp
-	mv tmp STATIS
-	./read_statis.sh > ene
-	cd ..
-fi
-
-if $moldy; then
-	echo "running moldy test (ex7)"
-	cd moldy
-	$EXEMOLDY control.argon > output.argon
-	./plotrdf output.argon > gr.moldy
-	awk 'BEGIN {lr=3} {if ((NF==13)&&($1!="Dip")&&(lr%3==0)) {print $0 } if ((NF==13)&&($1!="Dip")) {lr=lr+1} }' output.argon > ene
+		rm STATIS OUTPUT RDFDAT REVCON  REVIVE
+		$EXEDLPOLY
+		tail -n +4 RDFDAT > RDFDAT.$ensemble
 	cd ..
 fi
 
 if $mdff; then
 	echo "running mdff test (ex7)"
 	cd mdff
-	$EXEMDFF control_md.F > stdout_md
-	grep tot OSZIFF | awk '{print $3,$15}' > ene
-	$EXEMDFF control_gr.F > stdout_gr
+	$EXEMDFF control_md.F > stdout_md.$ensemble
+	$EXEMDFF control_gr.F > stdout_gr.$ensemble
+	mv GRTFF GRTFF.$ensemble
 	cd ..
 fi
-
-
-cat > plot.ene << eof
-#!/usr/bin/gnuplot -persist
-reset
-#-13921.0267036681
-p 'dl_poly/ene' w l,'mdff/ene' u 1:(\$2-13921.0267036681) w l,'moldy/ene' u 0:(\$3*120) w l
-eof
-chmod u+x plot.ene
-./plot.ene
 
 cat > plot.gr << eof
 #!/usr/bin/gnuplot -persist
 reset
-p 'dl_poly/RDFDAT' w l ,'mdff/GRTFF' w l,'moldy/gr.moldy' w l
+p 'dl_poly/RDFDAT.$ensemble' w l ,'mdff/GRTFF.$ensemble' w l
 eof
 chmod u+x plot.gr
 ./plot.gr
