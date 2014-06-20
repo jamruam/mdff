d1c5051074e9ebf7e979e15c8749fecc0e6a1e12
filipe.manuel.vasconcelos@gmail.com <filipe.manuel.vasconcelos@gmail.com@e3ff7af2-d1ba-a1ce-caeb-a33717494603>
2014-06-20 07:00:11 +0000

    update scripts

--- bin/random_struct.py
+++ bin/random_struct.py
@@ -4,10 +4,7 @@ import random
 from config import Config 
 from config import Ion 
 from optparse   import OptionParser
-
-# ================================================================================================================
-def foo_callback(option, opt, value, parser):
-    setattr(parser.values, option.dest, value.split(','))
+import sys
 
 atype=[]
 itype=[]
@@ -15,17 +12,10 @@ u=[]
 v=[]
 w=[]
 
-print 60*"="
-print "random_struct in box"
-print "author : fmv"
-print "email  : filipe.manuel.vasconcelos@gmail.com"
-print "email  : filipe.vasconcelos@cea.fr"
-print "date   : mars 2013 "
-print 60*"="
-
 # options parsing 
-usage = "usage: %prog [-a][-h]"
+usage = "usage: %prog --help [-h][-q][-n][-t][-i][-a][-b][-c]"
 parser = OptionParser(usage,version="%prog 0.1")
+parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=True,help="don't print status messages to stdout")
 # nions
 parser.add_option("-n", "--nions", type="int", dest="nions", help="number of ions in the box")
 # atype
@@ -38,13 +28,50 @@ parser.add_option("-a", "--acell", type="string", dest="su", help="a vector of t
 parser.add_option("-b", "--bcell", type="string", dest="sv", help="b vector of the box ")
 # w
 parser.add_option("-c", "--ccell", type="string", dest="sw", help="c vector of the box ")
+# cubic
+parser.add_option("--cubic", action="store_true", dest="cubic", default=False,help="cubic cell")
+# cell
+parser.add_option("--cell", type="float", dest="cell", default=None,help="cubic cell parameters")
 (options, args ) = parser.parse_args()
+
+
+if not options.nions :
+    parser.error('Number of ions needed --nions')
+
+if not options.satype :
+    parser.error('List of ion types name needed --atype')
+
+if not options.sitype :
+    parser.error('List of number of ions per type needed --itype')
+
+if options.cubic :
+    if not options.cell :
+         parser.error('Cubic cell parameters needed --cell')
+else:
+    if not options.su or not options.sv or not options.sw  :
+        parser.error('cell parameters needed --acell --bcell --ccell')
+
+
+if options.verbose :
+    print 60*"="
+    print "random_struct in box"
+    print "author : fmv"
+    print "email  : filipe.manuel.vasconcelos@gmail.com"
+    print "email  : filipe.vasconcelos@cea.fr"
+    print "date   : mars 2013 "
+    print 60*"="
 
 atype=options.satype.split()
 itype=[ int(item) for item in options.sitype.split() ]
-u    =[ float(item) for item in options.su.split() ]
-v    =[ float(item) for item in options.sv.split() ]
-w    =[ float(item) for item in options.sw.split() ]
+if options.cubic :
+    u    =[ options.cell, 0.0         , 0.0          ]
+    v    =[ 0.0         , options.cell, 0.0          ]
+    w    =[ 0.0         , 0.0         , options.cell ]
+else  :
+    u    =[ float(item) for item in options.su.split() ]
+    v    =[ float(item) for item in options.sv.split() ]
+    w    =[ float(item) for item in options.sw.split() ]
+
 ions=[]	
 conf = Config(ions=ions,u=u,v=v,w=w,system='randomPY',natm=options.nions,ntype=len(atype),types=atype, natmpertype = itype, coord_format='Direct')
 
@@ -61,7 +88,7 @@ for ia in range(conf.natm):
     conf.move(ia,pos=[x,y,z])
 
 conf.write_POSFF('POSFF.randomPY')
-
+print "POSFF.randomPY generated"
 
 
 
--- bin/read_quench_table.py
+++ bin/read_quench_table.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+# ===============================================================
+# name        : read_quench_table
+# author      : fmv
+# date        : somewhere in march-april 2014
+# description : this python script reads the quench.config global
+#               parameters and those related to the quench. Then 
+#               it generates "sed" scripts according to the input
+#               parameters together with the DOALLC DOALLMD exec-
+#               ution sequence.
+# ===============================================================
+
+
+# ------------------------------------------------
+# main function to read the quench.config file
+# it returns variables , newtable which are list of 
+# the main lines of quench.config.
+# variables : global parameters 
+#             for item in var:
+#               item[0] = name
+#               item[1] = value
+# newtable  : quench table
+# ------------------------------------------------
 def read_quench():
     
     f=open('quench.config')
@@ -37,16 +59,17 @@ def read_quench():
 
     return variables,newtable
 
-# ============================================================================
+# ------------------------------------------------
+# ------------------------------------------------
 def write_script(var,table):
 
+    # first write the sed script for the global parameters 
+    # no matters the equivalent parameter exists in the template 
     fout=open('scr','w')
     first=True
     k=1
     for item in var:
-
         if item[0] == "DT" : dt=float(item[1])
- 
         if k != len(var) :
             if first :
                 print >> fout, "sed -e s/__"+item[0]+"__/"+item[1]+"/g",
@@ -54,11 +77,10 @@ def write_script(var,table):
                 print >> fout, "    -e s/__"+item[0]+"__/"+item[1]+"/g",
         else:
             print >> fout, "    -e s/__"+item[0]+"__/"+item[1]+"/g tmp1 > tmp"
-            
         first=False
         k+=1
-
     fout.close()
+
 
     fdo=open('DOALLC','w')
     fdo2=open('DOALLMD','w')
@@ -202,7 +224,6 @@ def write_script(var,table):
 var=[]
 table=[]
 var,table = read_quench()
-
 write_script(var,table)
 
 
